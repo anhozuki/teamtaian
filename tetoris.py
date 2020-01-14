6e322a155c12380524410cf766234b177505752d
anhozuki <55684551+anhozuki@users.noreply.github.com>
2020-01-14 15:35:13 +0900

    Update tetoris.py

--- tetoris.py
+++ tetoris.py
@@ -83,7 +83,7 @@ class View():
             for j in range(Model.width):
                 x = 50+30*j
                 y = 70+30*i
-                self.canvas.create_text(x+300,y+15,text=self.model.data[i][j],font=("Helvetica",15,"bold"),fill="white",tag="block")
+                self.canvas.create_text(x+300,y+15,text=self.model.data[i][j],font=("Helvetica",15,"bold"),fill="",tag="block")
                 if self.model.data[i][j] == 1 or self.model.data[i][j] == 2: #dataが1か2のものを表示
                     self.canvas.create_rectangle(x,y,x+30,y+30,fill="red",outline="yellow",tag="block")
 
